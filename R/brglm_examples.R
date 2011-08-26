81b74a9778bd725c5b587019f33e46b01e4ebf3b
ioannis <ioannis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-26 15:00:30 +0000

    Corrected some typos in brglm_examples.R
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/brglm@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/brglm_examples.R
+++ R/brglm_examples.R
@@ -22,10 +22,10 @@ clotting <- data.frame(u = c(5,10,15,20,30,40,60,80,100),
 
 ## The bias-reduced fit. The bias-reduced estimate of the dispersion
 ## parameter is also calculated.
-fm1br <- update(m1, method = "brglm.fit")
+fm1br <- update(fm1, method = "brglm.fit")
 ## The bias-corrected fit. The bias-corrected estimate of the dispersion
 ## parameter is also calculated. The correction argument is from brglm.control
-fm1bc <- update(m1, method = "brglm.fit", correction = TRUE)
+fm1bc <- update(fm1, method = "brglm.fit", correction = TRUE)
 
 ## Now another bias-reduced fit. Bias is reduced for 1/dispersion
 ## instead of dispersion
@@ -36,11 +36,8 @@ fm1brInv <- glm(duration ~ fract + numst2, family = Gamma, data = coalition,
 fm1bcInv <- glm(duration ~ fract + numst2, family = Gamma, data = coalition,
                 method = "brglm.fit", dispTrans = "inverse", correction = TRUE)
 
-
-
-
 ### The lizards example from the previous version of brglm
-data(lizards)
+load("../data/lizards.rda")
 ## Fit the model using maximum likelihood
 lizardsML <- glm(cbind(grahami, opalinus) ~ height + diameter +
                  light + time, family = binomial(logit), data=lizards,
@@ -50,7 +47,7 @@ lizardsBR <- glm(cbind(grahami, opalinus) ~ height + diameter +
                  light + time, family = binomial(logit), data=lizards,
                  method = "brglm.fit")
 lizardsML
-≈lizardsBR
+lizardsBR
 ## Other links
 update(lizardsBR, family = binomial(probit))
 update(lizardsBR, family = binomial(cloglog))
