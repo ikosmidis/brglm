d1db79ade9228ce9251145b2f49f7e37973cfe78
ioannis <ioannis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-06-28 21:17:08 +0000

    Editted .onAttach message
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/brglm@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/brglm/R/aaa.R
+++ pkg/brglm/R/aaa.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("'brglm' will gradually be superseded by 'brglm2' (https://cran.r-project.org/package=brglm2), which provides comprehensive utilities for mean and median bias reduction for all GLMs and for the detection of infinite estimates in binomial-response models")
+  packageStartupMessage("'brglm' will gradually be superseded by 'brglm2' (https://cran.r-project.org/package=brglm2), which provides utilities for mean and median bias reduction for all GLMs and methods for the detection of infinite estimates in binomial-response models.")
 }
