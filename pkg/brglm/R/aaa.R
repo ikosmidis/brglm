3c5a43bd3f3daf499db873b20cd7892858a469ab
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2019-04-02 15:13:47 +0100

    cleaning up after importing form svn

--- pkg/brglm/R/aaa.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("'brglm' will gradually be superseded by 'brglm2' (https://cran.r-project.org/package=brglm2), which provides utilities for mean and median bias reduction for all GLMs and methods for the detection of infinite estimates in binomial-response models.")
-}
