f05bf4acc1e36c144dad8a91cc7f7a5ac2e37794
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2021-04-22 11:44:20 +0100

    documentation and message updates + depreciation warnings + 2CRAN

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("'brglm' will gradually be superseded by 'brglm2' (https://cran.r-project.org/package=brglm2), which provides utilities for mean and median bias reduction for all GLMs and methods for the detection of infinite estimates in binomial-response models.")
+  packageStartupMessage("'brglm' will gradually be superseded by the 'brglm2' R package (https://cran.r-project.org/package=brglm2), which provides utilities for mean and median bias reduction for all GLMs.\n Methods for the detection of separation and infinite estimates in binomial-response models are provided by the 'detectseparation' R package (https://cran.r-project.org/package=detectseparation).")
 }
--- R/separation.detection.R
+++ R/separation.detection.R
@@ -1,15 +1,16 @@
 `separation.detection` <-
-function (fit, nsteps = 30) 
-{
+function (fit, nsteps = 30)
+    {
+     .Deprecated(msg = "'separation.detection' will be removed from 'brglm' at version 0.8. Comprehensive methods for the detection of infinite estimates in binomial-response models are provided by the 'detectseparation' R package (https://cran.r-project.org/package=detectseparation).")
     fit.class <- class(fit)[1]
-    if (fit.class != "glm") 
+    if (fit.class != "glm")
         stop("Only objects of class 'glm' are accepted.")
     eps <- .Machine$double.eps
     betasNames <- names(betas <- coef(fit))
     noNA <- !is.na(betas)
     stdErrors <- matrix(0, nsteps, length(betas))
     for (i in 1:nsteps) {
-        suppressWarnings(temp.fit <- update(fit, control = glm.control(maxit = i, 
+        suppressWarnings(temp.fit <- update(fit, control = glm.control(maxit = i,
             epsilon = eps)))
         stdErrors[i, noNA] <- summary(temp.fit)$coef[betasNames[noNA], "Std. Error"]
     }
